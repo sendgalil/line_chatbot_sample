ae8332e83f79846567cc42f72f4b2337b54b7bf7
sendgalil <sendgalil01@gmail.com>
2018-10-11 01:18:15 +0800

    v0.1
    chatbot_sample v0.1

---
+++ chatbot_object.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 22 19:26:23 2018
+
+@author: Galil
+"""
+
+
+import json
+import requests
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+class chatbot(object):
+    def __init__(self, client_id, client_secret, handler, question_data_path, 
+                 response_data_path):
+        with open(question_data_path) as f:
+            question_data = json.load(f)
+        with open(response_data_path) as f:
+            response_data = json.load(f)
+        short_token = self.get_short_token(client_id, client_secret)
+        self.line_bot_api = LineBotApi(short_token)
+        self.handler = WebhookHandler(handler)
+        self.template_data = self.create_template(question_data)
+        self.response_data = self.create_response(response_data)
+        self.user_list = {}
+    #Methods
+    def get_short_token(self, client_id, client_secret):
+        #獲得短期的token (30天)
+        post_data = {'grant_type': 'client_credentials', 
+                'client_id': client_id, 
+                'client_secret': client_secret}
+        post_headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        r = requests.post('https://api.line.me/v2/oauth/accessToken', 
+                          data = post_data, headers = post_headers)
+        temp_json = json.loads(r.text)
+        token = temp_json['access_token']
+        return token
+    def create_template(self, question_data):
+        #依據不同的框架類型產生對應框架
+        template_data = {} 
+        for thing in question_data:
+            if (thing['template_type'] == 'ImageCarousel_template'):
+                temp_columns = []                
+                for column in thing['columns']:
+                    temp = ImageCarouselColumn(
+                                image_url=column['image_url'],
+                                action=PostbackAction(
+                                    label=column['label'],
+                                    data=column['data']
+                                )
+                            )
+                    temp_columns.append(temp)
+                template_message = TemplateSendMessage(
+                    alt_text='ImageCarousel template',
+                    template=ImageCarouselTemplate(
+                        columns=temp_columns
+                    )
+                )
+                template_data[thing['key_word']] = template_message
+            elif (thing['template_type'] == 'Text_message'):
+                template_data[thing['key_word']] = TextSendMessage(text=thing['text'])
+        return template_data
+    def create_response(self, temp_response_data):
+        response_data = {}
+        for thing in temp_response_data:
+            if (thing['type'] == 'text'):
+                response_message = TextSendMessage(text=thing['text'])
+                response_data[thing['key_word']] = response_message
+        return response_data
+    def create_task_template(self, uid, task):
+        task_template = {'task':'',
+                         'report_message':''}
+        #依據自定義的任務創建回覆的框架訊息
+        if task == '建議與問題回報':            
+            task_template['task'] = 'problem_return_and_advice'
+            self.user_list[uid] = task_template
+            response_text = ('感謝您的使用，請直接輸入您的問題或是建議，想離開的話'
+                            '請按結束回報即可')
+            response_template = TemplateSendMessage(
+                                alt_text='Carousel template',
+                                template=CarouselTemplate(
+                                    columns=[
+                                        CarouselColumn(
+                                            title='建議與問題回報',
+                                            text=response_text,
+                                            actions=[
+                                                PostbackAction(
+                                                    label='結束回報',
+                                                    data='message_type=end_of_return'
+                                               )
+                                            ]
+                                        )
+                                    ]
+                                )
+                            )
+        elif task == '搜尋資料':
+            task_template['task'] = 'search'
+            self.user_list[uid] = task_template
+            response_text = '請直接輸入關鍵字即可搜尋'
+            response_template = TextSendMessage(text=response_text)
+        
+        return response_template
---
+++ chatbot_smaple.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 17:05:18 2018
+
+@author: Galil
+"""
+
+#!/usr/bin/python
+#-*-coding:utf-8 -*
+import os
+from flask import Flask,url_for,request
+from urllib.parse import parse_qs
+from chatbot_object import *
+
+app = Flask(__name__)
+#起始圖文選單的文字
+original_menu = ['sample_1', 'sample_2', 'sample_3', 'about_me']
+task_meanu = ['serach', 'suggestion']
+question_data_path = 'question_data.json'
+response_data_path = 'answer_data.json'
+
+test_chatbot = chatbot('your_client_id',
+                       'your_client_secret',
+                       'your_handler',
+                       question_data_path,
+                       response_data_path)
+
+
+@app.route('/')
+def hello_world():
+    msg = "機械人測試中 版本:v0.1"
+    return msg
+
+@app.route('/callback',methods=["POST"])
+def callback():
+    temp = request.get_json()
+    print(temp)
+    for i in temp['events']:
+	  #取得回覆token,一個回覆只能用一次		
+        reply_token =  i['replyToken']
+        #取得uid 
+        uid = i['source']['userId']
+        if i['type']=='postback': #判斷的參數是哪個
+            post=parse_qs(i['postback']['data'])
+            """訊息傳送一率是使用postback
+               data裡的message_type有兩種值,用以表示tag的類型
+               1.question:問題類型,代表其tag是某組問題的關鍵字
+               2.answer:答案類型,代表其tag是某組答案的關鍵字"""
+            if post['message_type'][0] == 'question':
+                tag = post['tag'][0]
+                for key_word, reply_msg in test_chatbot.template_data.items():
+                    if key_word == tag:
+                        test_chatbot.line_bot_api.reply_message(reply_token,
+                                                                reply_msg)
+                        break
+            elif post['message_type'][0] == 'answer':
+                tag = post['tag'][0]
+                for key_word, reply_msg in test_chatbot.response_data.items():
+                    if key_word == tag:
+                        test_chatbot.line_bot_api.reply_message(reply_token,
+                                                                reply_msg)
+                        break
+            elif post['message_type'][0] == 'end_of_return':
+                print('回報的訊息:',test_chatbot.user_list[uid]['report_message'])
+                del test_chatbot.user_list[uid]
+                reply_msg = TextSendMessage(text='感謝您的建議')
+                test_chatbot.line_bot_api.reply_message(reply_token,
+                                                        reply_msg)
+        elif i['message']['type']=='text':
+            """判斷使用者是否在某項狀態中 若沒有則依據訊息進行回應
+               因為起始的圖文選單訊息都是text 所以設定一組關鍵字
+               只要在關鍵字內的文字訊息就對應相關功能或是回應相關訊息"""
+            msg = i['message']['text']
+            if uid not in test_chatbot.user_list.keys():
+                if msg in original_menu:
+                    for key_word, reply_msg in test_chatbot.template_data.items():
+                        if key_word == msg:
+                            test_chatbot.line_bot_api.reply_message(reply_token,
+                                                                     reply_msg)
+                            break
+                elif msg in task_meanu:
+                    task = msg
+                    reply_msg = test_chatbot.create_task_template(uid,task)
+                    test_chatbot.line_bot_api.reply_message(reply_token,reply_msg)
+            #依據不同的任務進行回應
+            #problem_return_and_advice
+            elif uid in test_chatbot.user_list.keys():
+                if test_chatbot.user_list[uid]['task'] == 'suggestion':
+                    test_chatbot.user_list[uid]['report_message'] += msg
+                elif test_chatbot.user_list[uid]['task'] == 'search':
+                    del test_chatbot.user_list[uid]
+                    tag = msg
+                    for key_word, reply_msg in test_chatbot.response_data.items():
+                        if key_word == tag:
+                            test_chatbot.line_bot_api.reply_message(reply_token,
+                                                                    reply_msg)
+                            break
+            #例外處理的回應            
+            else:
+                reply_msg = TextSendMessage(text='若要查詢資料請透過功能選單選擇喔~')
+                test_chatbot.line_bot_api.reply_message(reply_token,
+                                                        reply_msg)
+    return ""
+    
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
